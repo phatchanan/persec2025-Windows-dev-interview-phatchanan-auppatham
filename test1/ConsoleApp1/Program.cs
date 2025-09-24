a997679e5d304c9c2e935492f808362270502f45
P@Birdsaly <phatchanan@msn.com>
2025-09-24 11:00:29 +0700

    Add files via upload
    test1

---
+++ test1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+  static bool IsValid(string s)
+  {
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> pairs = new Dictionary<char, char>()
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+    foreach (char c in s)
+    {
+      if (c == '(' || c == '[' || c == '{')
+      {
+        stack.Push(c);
+      }
+      else if (c == ')' || c == ']' || c == '}')
+      {
+        if (stack.Count == 0 || stack.Peek() != pairs[c])
+          return false;
+        stack.Pop();
+      }
+    }
+
+    return stack.Count == 0;
+  }
+
+  static void Main()
+  {
+    string[] tests = { "()", "([])", "({[]})", "([{]])", ")", "(})", "([)", "{" };
+    foreach (var t in tests)
+    {
+      Console.WriteLine($"{t} => {IsValid(t)}");
+    }
+  }
+}
